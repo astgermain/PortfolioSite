93276819fcbe0ba2e14b803bd74a09b60f63984e
Andrew St Germain <roarkyo@gmail.com>
2020-05-25 14:49:33 -0700

    removing hostname from fetch

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -23,7 +23,7 @@ export const registerUser = (userData, history) => dispatch => {
 export const loginUser = userData => dispatch => {
     let url = window.location.hostname  
     axios
-        .post('https://' + url + ':4000/api/users/login', userData , { crossdomain: true })
+        .post('/api/users/login', userData)
         .then(res => {
         // Save to localStorage
         // Set token to localStorage
