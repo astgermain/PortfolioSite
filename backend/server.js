5050b4e564de170e6d9e94acc34339d5a1ef88e1
Andrew St Germain <roarkyo@gmail.com>
2020-05-25 14:07:12 -0700

    complete revert

--- backend/server.js
+++ backend/server.js
@@ -30,18 +30,6 @@ app.use('/api/users', validationRouter)
 app.use(passport.session())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  
   res.locals.loggedIn = req.isAuthenticated()
   next()
 })
