85bf18cdaa504788311d0506ab854939179f488b
Andrew St Germain <roarkyo@gmail.com>
2020-05-25 13:52:32 -0700

    Allowing api calls

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/users', validationRouter)
 app.use(passport.session())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
