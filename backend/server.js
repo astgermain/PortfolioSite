40be85c6cd6914933b9b74e031eb6ae3c8d21e27
Andrew St Germain <roarkyo@gmail.com>
2020-05-25 14:40:42 -0700

    acao update

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.use('/api/users', validationRouter)
 app.use(passport.session())
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.locals.loggedIn = req.isAuthenticated()
   next()
 })
