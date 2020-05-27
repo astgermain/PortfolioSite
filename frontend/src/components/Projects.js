9223bbfd047ac2460d883800e2209c8137347bbf
Andrew St Germain <roarkyo@gmail.com>
2020-05-27 14:25:27 -0700

    Pushing project changes without css update

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -5,8 +5,40 @@ import Jump from 'react-reveal/Jump';
 
 
 class Projects extends Component {
+
     state = {
-        opened: false
+        opened: false,
+        data: [],
+        id: 0,
+        message: null,
+        intervalIsSet: false,
+        idToDelete: null,
+        idToUpdate: null,
+        objectToUpdate: null,
+        name: null,
+        link: null,
+        image: null,
+        about: null,
+    };
+
+    // when component mounts, first thing it does is fetch all existing data in our db
+    // then we incorporate a polling logic so that we can easily see if our db has
+    // changed and implement those changes into our UI
+    componentDidMount() {
+        this.getDataFromDb();
+        
+    }
+    
+    // never let a process live forever
+    // always kill a process everytime we are done using it
+    componentWillUnmount() {
+        
+    }
+    getDataFromDb = () => {
+        fetch('/api/project')
+            .then((data) => data.json())
+            .then((res) => this.setState({ data: res.data }))
+            .catch(err => this.setState({ data: [] }))
     };
 
     onLogoutClick = e => {
@@ -18,6 +50,15 @@ class Projects extends Component {
         this.setState({ opened: !this.state.opened});
     }
     render() {
+        let data = this.state.data
+        let dl = 0
+        try{
+            dl = data.length
+        }
+        catch{
+            dl = 0
+        }
+
         return (
             <Jump>
                 <div className="project">
@@ -30,6 +71,20 @@ class Projects extends Component {
                     <h2 className="subHeader">
                         If there is no link to source code the project is under NDA, email me for a snippet.
                     </h2>
+                    {dl <= 0
+                    ? 'No Projects yet'
+                    //should map to state and let it rerender itself
+                    : data.map((dat) => (
+                        <li key={dat._id} style={{ padding: '10px' }}>
+                        <span style={{ color: 'gray' }}> name: </span> {dat.name} <br />
+                        <span style={{ color: 'gray' }}> link: </span> {dat.link} <br />
+                        <span style={{ color: 'gray' }}> image: </span> {dat.image} <br />
+                        <span style={{ color: 'gray' }}> about: </span> {dat.about} 
+                        <button onClick={() => this.deleteFromDB(dat._id)}>
+                            DELETE
+                        </button>
+                        </li>
+                    ))}
                 </div>
             </Jump>
     );
