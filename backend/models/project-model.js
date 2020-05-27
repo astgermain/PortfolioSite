88249c974b66dbf040e9ffcf565faa0e3f5427df
Andrew St Germain <roarkyo@gmail.com>
2020-05-27 14:13:04 -0700

    Small backend change for project model

--- backend/models/project-model.js
+++ backend/models/project-model.js
@@ -6,6 +6,7 @@ const Project = new Schema(
         name: { type: String, required: true },
         link: { type: String, required: true },
         image: { type: String, required: true },
+        about: { type: String, required: true },
     },
     { timestamps: true },
 )
