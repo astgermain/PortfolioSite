c66719961f04a6ddd5a5684a28528eb93eaa01f7
Andrew St Germain <roarkyo@gmail.com>
2020-05-28 18:07:41 -0700

    Forgot to open ports on EC2

--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,6 @@ var express = require("express");
 var morgan = require("morgan");
 var compression = require('compression');
 var helmet = require('helmet');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 var app = express();
 app.use(helmet());
@@ -19,16 +17,7 @@ app.get("*", function(req, res){
   res.sendFile(__dirname + "/build/index.html");
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('User Disconnected');
-  });
-  socket.on('example_message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-io.listen(8000)
+
 
 // Listen to port 3000
 app.listen(3000);
