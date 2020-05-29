dc6617d45fe632daef69b4eed9e8aa62cab4d95b
Andrew St Germain <roarkyo@gmail.com>
2020-05-28 18:44:13 -0700

    Fixed frontend server settings

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var morgan = require("morgan");
-var compression = require('compression');
-var helmet = require('helmet');
-
+const express = require("express");
+const morgan = require("morgan");
+const compression = require('compression');
+const helmet = require('helmet');
+const app = express()
+const server = http.createServer(app)
 const io = require('socket.io')(server, {
   path: '/chat/socket.io'
 });
@@ -12,31 +13,6 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
-  });
-
-  socket.on('new-channel-added', channel => {
-    console.log(channel);
-    socket.broadcast.emit('new-channel-added-broadcast-from-server', channel);
-  });
-  
-  socket.on('channel-deleted', channelId => {
-    socket.broadcast.emit('channel-deleted-broadcast-from-server', channelId);
-  });
-
-  socket.on('joined-channel', data => {
-    socket.broadcast.emit('joined-channel-broadcast-from-server', data);
-  });
-  
-  socket.on('left-channel', data => {
-    socket.broadcast.emit('left-channel-broadcast-from-server', data);
-  });
-
-  socket.on('new-message-added', message => {
-    socket.broadcast.emit('new-message-added-broadcast-from-server', message);
-  });
-
-  socket.on('message-deleted', messageId => {
-    socket.broadcast.emit('message-deleted-broadcast-from-server', messageId);
   });
 });
 
